94333c7f0e461b1f106607e4da51ac2deabac7dc
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-09-12 17:16:24 -0300

    Remove allMathOperator template and convertion script

--- src/syntax/script.js
+++
@@ -1,8 +0,0 @@
-const allMathOperators = require('./allMathOperators');
-
-const allMathOperatorsByChar = allMathOperators.reduce(
-  (acc, val) => Object.assign({ [val.character]: val.latex }, acc),
-  {},
-);
-
-console.log(JSON.stringify(allMathOperatorsByChar));

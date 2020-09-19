c83ec6e3387ecf71f7f99458c354821e0ee2840b
Alexandre Nunes <alexandrenunes@Alexandres-MacBook-Pro.local>
2020-09-19 13:57:18 -0300

    refactor: remove all math symbols original file and script

--- src/syntax/scripts.js
+++
@@ -1,8 +0,0 @@
-const allMathOperators = require('./allMathSymbols');
-
-const allMathOperatorsByChar = allMathOperators.reduce(
-  (acc, val) => Object.assign({ [val.glyph]: val.latex }, acc),
-  {},
-);
-
-console.log(JSON.stringify(allMathOperatorsByChar));

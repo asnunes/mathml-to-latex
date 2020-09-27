1b78ee7ca1869055947dfc4d62c8aa4f442f6363
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-09-27 02:23:15 -0300

    chore: add and fix prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
   },
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {},
 };
